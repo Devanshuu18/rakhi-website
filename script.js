84e6725086f733e2b332dd1c94c13df5ffdc663d
Devanshuu18 <b240741@skit.ac.in>
2025-08-05 23:22:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function showMore() {
+  const surpriseText = document.getElementById("surprise-text");
+  surpriseText.innerText = "No matter how much we fight, you'll always be my forever favorite! 🥰";
+  surpriseText.style.display = "block";
+}
